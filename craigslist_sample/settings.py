27b9b2bd66551461bc2c1089ef5dd327527744a2
Andre <andresony@gmail.com>
2016-07-14 18:43:17 +0100

    changing to thumbnail

--- craigslist_sample/settings.py
+++ craigslist_sample/settings.py
@@ -12,14 +12,21 @@ DOWNLOAD_HANDLERS = {'s3': None}
 
 SPIDER_MODULES = ['craigslist_sample.spiders']
 NEWSPIDER_MODULE = 'craigslist_sample.spiders'
-ITEM_PIPELINES = {'craigslist_sample.pipelines.Mp4Pipeline': 1,
-                  'craigslist_sample.pipelines.XmlExportPipeline': 2}
+ITEM_PIPELINES = {'craigslist_sample.pipelines.Mp4Pipeline': 1#,
+                  #'craigslist_sample.pipelines.XmlExportPipeline': 2
+                  }
 DOWNLOADER_MIDDLEWARES = {
     'craigslist_sample.middlewares.PhMiddleware': 543,
 }
 FILES_STORE = './data/'
 FILES_EXPIRES = 7
 SAMPLE_INTERVAL_SEC = 5
-VIDEO_RESOLUTION = '240p' # 240p,480p,720p,1080p
+VIDEO_RESOLUTION = '240' # 240p,480p,720p,1080p
+COOKIES_ENABLED = False
+DOWNLOAD_DELAY = 1
+DOWNLOAD_MAXSIZE = 257741824
+DOWNLOAD_WARNSIZE = 107741824
+DOWNLOAD_TIMEOUT = 600
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'craigslist_sample (+http://www.yourdomain.com)'
--- craigslist_sample/spiders/ph.py
+++ craigslist_sample/spiders/ph.py
@@ -5,7 +5,9 @@ from scrapy.linkextractors import LinkExtractor
 from craigslist_sample.items import PhItem
 import re
 from demjson import decode
+from math import ceil
 import craigslist_sample.settings
+from craigslist_sample.settings import VIDEO_RESOLUTION
 
 class MySpider(CrawlSpider):
     name = "ph"
@@ -14,9 +16,10 @@ class MySpider(CrawlSpider):
                        "cdnt4b.video.pornhub.phncdn.com",
                        "cdn1.video.pornhub.phncdn.com",
                        "cdn2b.video.pornhub.phncdn.com",
-                       "[a-z|0-9].rncdn3.com",
-                       "[a-z|0-9]*.video.pornhub.phncdn.com",]
-    start_urls = ["http://www.pornhub.com/channels/brazzers/videos?o=da",
+                       "[0-9|a-z|A-Z|.]*.rncdn3.com",
+                       ".*.video.pornhub.phncdn.com",]
+    start_urls = ["http://www.pornhub.com/video?c=41",
+                  #"http://www.pornhub.com/channels/povd/videos?o=vi",
                   #"http://www.pornhub.com/channels/povd/videos?o=ra"
                   ]
 
@@ -24,32 +27,54 @@ class MySpider(CrawlSpider):
     #    Rule(LinkExtractor(allow=(), restrict_xpaths=('//li[@class="page_next"]',)), callback="parse", follow=True),
     #)
 
+    def generate_file_urls(self,item):
+        num_urls = int(ceil(float(item["duration"])/float(item["thumbsFrequency"])/25.0))
+        file_urls = []
+        pattern = item["thumbnails"]
+        print pattern
+        for i in range(num_urls):
+            url = re.sub("S\{[0-9]*\}", ("S"+str(i)), pattern)
+            print url
+            file_urls.append(url)
+        return file_urls
+
+
+
     def parse_video(self, response):
         hxs = Selector(response)
 
         item = PhItem()
         item["link"] = response.url
-        item["id"] = hxs.xpath('//div[@class="video-wrapper"]/div/@data-video-id').extract()
-        item["title"] = hxs.xpath('//title').extract()
+        item["viewkey"] = re.search("viewkey=(.*)", response.url).group(1)
+        item["id"] = hxs.xpath('//div/@data-video-id').extract()[0]
+        item["title"] = hxs.xpath('//title').extract()[0]
         item["duration"] = hxs.xpath('//div/@data-duration').extract()[0]
-        jscode = hxs.xpath('//div[@id="player"]/script[@type="text/javascript"]').extract()
+        jscode = hxs.xpath('//div[@id="player"]/script[@type="text/javascript"]').extract()[0]
         if not jscode == []:
             #download_url = re.search("var player_quality_"+settings.VIDEO_RESOLUTION+" = '(.*)';", jscode[0]).group(1).split(";")[0]
-            download_url = re.search("var player_quality_240p = '(.*)';", jscode[0]).group(1).split(";")[0]
-            if download_url:
-                item["file_urls"] = [download_url.replace("'","")]
-                jscode = hxs.xpath('//div[@class="video-wrapper"]/div/script[@type="text/javascript"]').extract()
-                flash_vars = re.search("var flashvars_[0-9]* = (\{.*\});",jscode[0]).group(1)
-                jsonvars = decode(flash_vars)
-                if "actionTags" in jsonvars:
-                    tags = jsonvars["actionTags"]
-                    if tags:
-                        item["tags"] = tags
+            #download_url = re.search("var player_quality_"+VIDEO_RESOLUTION+"p = '(.*)';", jscode).group(1).split(";")[0]
+            #if download_url and VIDEO_RESOLUTION in download_url:
+            #    item["file_urls"] = [download_url.replace("'","")]
+            jscode = hxs.xpath('//div[@class="video-wrapper"]/div/script[@type="text/javascript"]').extract()[0]
+            flash_vars = re.search("var flashvars_[0-9]* = (\{.*\});",jscode).group(1)
+            jsonvars = decode(flash_vars)
+            if "actionTags" in jsonvars:
+                tags = jsonvars["actionTags"]
+                if tags:
+                    item["tags"] = tags
+                    item["thumbnails"] = jsonvars["thumbs"]["urlPattern"]
+                    item["thumbsFrequency"] = jsonvars["thumbs"]["samplingFrequency"]
+                    height = int(jsonvars["thumbs"]["thumbHeight"])
+                    width = int(jsonvars["thumbs"]["thumbWidth"])
+                    if height==90 and width>=160 and item["thumbnails"]:
+                        item["file_urls"] = self.generate_file_urls(item)
                         yield item
 
     def parse(self, response):
         hxs = Selector(response)
         videos = hxs.xpath('//li[@class="videoblock"]/div/div/a')
+        if not videos:
+            videos = hxs.xpath('//div[@class="phimage"]/a')
         for video in videos:
             url = response.urljoin(video.xpath("@href").extract()[0])
             yield Request(url, callback=self.parse_video, method="GET")
